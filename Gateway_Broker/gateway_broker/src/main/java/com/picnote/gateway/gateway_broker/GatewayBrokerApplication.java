e7db5b909061dd8beca9c088ecf81b470187a9f6
prithviemmanuelmachado <prithviemmanuelmachado@gmail.com>
2023-10-04 21:22:20 +0530

    redo the project setup

---
+++ Gateway_Broker/gateway_broker/src/main/java/com/picnote/gateway/gateway_broker/GatewayBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.picnote.gateway.gateway_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayBrokerApplication.class, args);
+	}
+
+}
---
+++ Gateway_Broker/gateway_broker/src/test/java/com/picnote/gateway/gateway_broker/GatewayBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picnote.gateway.gateway_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
